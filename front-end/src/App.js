eb2081c3bed73d19c86b108526de48351e39291f
Monica Anisetty <Monica.Anisetty@BRILLIO.COM>
2021-07-15 11:59:36 +0530

    first Front-end commit

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Routercomp from './Components/Routercomp';
+
+function App() {
+  return (
+    <div className="App">
+      <Routercomp/>
+    </div>
+  );
+}
+
+export default App;
